8d79f38611b2867debc13b123b012128cc08e53c
Numan Anees <numananees44@gmail.com>
2022-06-21 16:07:11 +0500

    Header & Banner created

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+import img from "../assets/img/house-banner.png";
+import Search from "./Search";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section className="h-full max-h-[640px] mb-8 xl:mb-24">
+      <div className="flex flex-col lg:flex-row">
+        <div className="lg:ml-8 xl:ml-[135px] flex  flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 ">
+          <h1>
+            <span>Rent</span>Your Dream House With Us.
+          </h1>
+          <p>
+            In ullamco laborum esse incididunt labore anim aliquip nostrud velit
+            culpa excepteur. Mollit eu commodo consequat laboris amet commodo.
+            Culpa proident amet consequat exercitation laboris veniam aliquip
+            dolore. Eu deserunt incididunt aliqua nostrud commodo qui minim
+            fugiat officia et laborum quis.
+          </p>
+        </div>
+        <div>
+          <img src={img} alt="img" />
+        </div>
+      </div>
+      <Search />
+    </section>
+  );
 };
 
 export default Banner;
