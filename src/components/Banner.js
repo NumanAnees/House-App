dabdf6b55d5d32c45a91dd226f839e3ffe345bbe
Numan Anees <numananees44@gmail.com>
2022-06-21 21:23:54 +0500

    Dropdowns created

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,11 +6,12 @@ const Banner = () => {
   return (
     <section className="h-full max-h-[640px] mb-8 xl:mb-24">
       <div className="flex flex-col lg:flex-row">
-        <div className="lg:ml-8 xl:ml-[135px] flex  flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 ">
-          <h1>
-            <span>Rent</span>Your Dream House With Us.
+        <div className="lg:ml-8 xl:ml-[135px] flex  flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0">
+          <h1 className="text-4xl lg:text-[58px] font-semibold leading-none mb-6">
+            <span className="text-violet-700">Rent</span> Your Dream House With
+            Us.
           </h1>
-          <p>
+          <p className="max-w-[480px] mb-8">
             In ullamco laborum esse incididunt labore anim aliquip nostrud velit
             culpa excepteur. Mollit eu commodo consequat laboris amet commodo.
             Culpa proident amet consequat exercitation laboris veniam aliquip
@@ -18,7 +19,7 @@ const Banner = () => {
             fugiat officia et laborum quis.
           </p>
         </div>
-        <div>
+        <div className="hidden flex-1 lg:flex justify-end items-end">
           <img src={img} alt="img" />
         </div>
       </div>
--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from "react";
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+//Dummy Data
+import { housesData } from "../data";
+
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState("Location (Any)");
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState("Property type (Any)");
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState("Price range (Any)");
+  const [loading, setLoading] = useState(false);
+  //return all counrties
+  useEffect(() => {
+    const allCountries = houses.map((house) => {
+      return house.country;
+    });
+    //remove duplicates
+    const uniqueCountries = ["Location (any)", ...new Set(allCountries)];
+    setCountries(uniqueCountries);
+  }, []);
+  return (
+    <HouseContext.Provider
+      value={{
+        country,
+        setCountry,
+        countries,
+        property,
+        setProperty,
+        properties,
+        price,
+        setPrice,
+        houses,
+        loading,
+      }}
+    >
+      {children}
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
